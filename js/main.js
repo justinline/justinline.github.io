e56b449af9e51c5e3ca6cce31231dab61326cce2
Justin Focus <iamjustinfocus@gmail.com>
2017-05-08 00:36:17 +0100

    added 'personal' section

--- js/main.js
+++ js/main.js
@@ -11,6 +11,9 @@ function checkCommand(command) {
 		<li class="line">
 			<span class="title">~* TERMINAL COMMANDS *~</span>
 			<br/><br/>
+			<span class="highlight">justin</span> <br/>
+			<small>--> Prints Personal Details</small>
+			<br/><br/>
 			<span class="highlight">cv</span> <br/>
 			<small>--> Prints current CV to screen</small>
 			<br/><br/>
@@ -28,13 +31,46 @@ function checkCommand(command) {
 			<br/>
 		</li>
 		`;
+	} else if (command == 'justin'){
+		terminal.innerHTML += `
+		<li class="line">
+		<br/>
+		</li>		
+		<li class="line">
+			<span class="title">~* WHO IS HE? *~</span>
+			<br/><br/>
+			<img src="../images/portrait.jpg" alt="Justin Focus" className="src"/>			
+			<br/><br/>
+			<span class="highlight">Justin Focus</span> <br/>
+			<small>Age: 27 | Current Occupation: Technical Designer</small>
+			<br/><br/>
+			<span class="highlight">Nationality</span> <br/>
+			<small>British/European</small>
+			<br/><br/>			
+			<span class="highlight">Location</span> <br/> 
+			<small>London</small>
+			<br/><br/>
+			<span class="highlight">Summary</span> <br/>
+			<small>Currently, I am looking to move into a development focused career: to push my
+self-development, fuel my desire to learn and hopefully work on projects that have
+a great social impact. One thing I enjoy more than working on a project is seeing
+people using whatever it is I have built.</small>
+			<br/><br/>
+			<span class="highlight">Employ Me?</span> <br/>
+			<small><a href="mailto:iamjustinfocus@gmail.com">iamjustinfocus@gmail.com</a></small>
+			<br/><br/>
+		</li>
+		<li class="line">
+			<br/>
+		</li>
+		`;
 	} else if (command == 'skills'){
 		terminal.innerHTML += `
 		<li class="line">
 			<br/>
 		</li>		
 		<li class="line">
-			<span class="title">~* SKILLS AND KNOWLEDGE *~</span>:
+			<span class="title">~* SKILLS AND KNOWLEDGE *~</span>
 			<br/><br/>
 			<span class="highlight">Languages</span><br/>
 			Python, Javascript, HTML, CSS <br/>
@@ -55,7 +91,7 @@ function checkCommand(command) {
 			<br/>
 		</li>		
 		<li class="line">
-			<span class="title">~* JUSTIN FOCUS'S TERMINAL CV *~</span>:
+			<span class="title">~* EMPLOYMENT AND EDUCATIION *~</span>
 			<br/><br/>
 			<a href="../assets/justinline-cv.pdf" download>DOWNLOAD PDF HERE</a><br/><br/>
 			<span class="highlight">Education</span><br/>
@@ -81,7 +117,7 @@ function checkCommand(command) {
 			<br/>
 		</li>		
 		<li class="line">
-			<span class="title">~* CONTACT ME *~</span>:
+			<span class="title">~* CONTACT ME *~</span>
 			<br/><br/>
 			<span class="highlight">Email:</span><br/>
 			<a href="mailto:iamjustinfocus@gmail.com">iamjustinfocus@gmail.com</a>
